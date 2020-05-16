58ea9dc489380127ed0c64d526e004ff5c94223b
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-05-16 12:17:30 +0200

    fix parenthesis

--- watch.js
+++ watch.js
@@ -18,14 +18,14 @@ const displayTimer = async (videoDuration, adsDuration, color) => {
 
     for (let time = 0; time <= adsDuration; time++) {
         await timeout(1000)
-        if (time % 5 === 0 || time === adsDuration) {
+        if ((time % 5 === 0) || time === adsDuration) {
             console.log(color, 'Ads : Watched ' + time + 's / ' + adsDuration + 's')
         }
     }
     
     for (let time = 0; time <= videoDuration; time++) {
         await timeout(1000)
-        if (time % 5 === 0 || time === videoDuration) {
+        if ((time % 5 === 0) || time === videoDuration) {
             console.log(color, 'Video : Watched ' + time + 's / ' + videoDuration + 's')
         }
     }
